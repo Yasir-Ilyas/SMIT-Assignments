0dea8673d36bef7375392f2cbd0d328d371c7e3a
Yasir <yasir.libra93@gmail.com>
2023-12-31 11:17:36 -0800

    Add files via upload

---
+++ Q2.dart
@@ -0,0 +1,16 @@
+void main(){
+    int john = 23;
+    int doe = 21;
+
+    if(john > doe){
+        print('John is older than Doe');
+    }
+    else if (john==doe)
+            {
+            print('John is the same age as of doe');
+            }
+
+    else{
+        print('John is younger than doe');
+    }
+}
