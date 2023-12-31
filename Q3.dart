29b82c9fc1d19822c960f1e4e35c46875291a677
Yasir <yasir.libra93@gmail.com>
2023-12-31 11:27:54 -0800

    Add files via upload

---
+++ Q3.dart
@@ -0,0 +1,20 @@
+void main(){
+
+    int classesHeld = 16;
+    int classesAttended = 10;
+    double ratio = 10/16;
+    double percentage = ratio*100;
+    print(percentage);
+
+    if(ratio > 0.75){
+        print('Allowed to sit in exam');
+    }
+
+    else{
+        print('Not allowed to sit in exam');
+    }
+
+
+
+
+}
