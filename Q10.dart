595f35d0bb6303aa3002a3ce9a6fdef01922ba24
Yasir <yasir.libra93@gmail.com>
2024-01-08 03:20:09 -0800

    Add files via upload

---
+++ Q10.dart
@@ -0,0 +1,18 @@
+void main(){
+    int a =2;
+    int b =3;
+    int c =4;
+    if(a <b && a<c )
+    {
+        print('a is the lowest number');
+    }
+    else if(b<c && b<a)
+    {
+        print('b is lowest number');
+    }
+
+        else if(c<a && c<b)
+    {
+        print('c is lowest number');
+    }
+}
