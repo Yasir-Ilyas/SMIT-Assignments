007cf009fd8823bb6eee1f3bcf2258b01e89942b
Yasir <yasir.libra93@gmail.com>
2024-01-08 03:34:09 -0800

    Add files via upload

---
+++ Q7.dart
@@ -0,0 +1,12 @@
+//Square root program
+void main(){
+    double number = 2;
+    if(number > 0){
+        double squareRoot = sqrt(number);
+        print(squareRoot);
+    }
+        else {
+            print('Please enter number greater than 0');
+        }
+    }
+}
