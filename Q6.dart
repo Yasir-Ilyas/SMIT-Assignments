7e97b689afbf1dd07de13241a407c124e8d30e43
Yasir <yasir.libra93@gmail.com>
2024-01-08 03:05:15 -0800

    Add files via upload

---
+++ Q6.dart
@@ -0,0 +1,10 @@
+void main(){
+    String alphabet = 'a' ;
+    if (alphabet == 'a' || alphabet =='e'  || alphabet == 'i' || alphabet == 'o' || alphabet =='u')
+    {
+        print('alphabet is vowel');
+    }
+    else{
+        print('alphabet is not vowel');
+    }
+}
